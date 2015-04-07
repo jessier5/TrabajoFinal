66ddc8ba4eead6976cdca4d586c6f86ae0fbc4f1
alyer <alyer522@hotmail.com>
2015-04-07 21:56:39 +0200

    cambiando el nombre del LugarBean a Lugar

---
+++ EjercicioFinal/src/es/exitae/ejerciciofinal/beans/Lugar.java
@@ -0,0 +1,49 @@
+package es.exitae.ejerciciofinal.beans;
+
+public class Lugar {
+	
+	// definimos los atributos del bean
+	private int 		id;
+	private String nombreLugar;
+	private String descrLugar;
+	private float   latitud;
+	private float 	longitud;
+	private String foto;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNombreLugar() {
+		return nombreLugar;
+	}
+	public void setNombreLugar(String nombreLugar) {
+		this.nombreLugar = nombreLugar;
+	}
+	public String getDescrLugar() {
+		return descrLugar;
+	}
+	public void setDescrLugar(String descrLugar) {
+		this.descrLugar = descrLugar;
+	}
+	public float getLatitud() {
+		return latitud;
+	}
+	public void setLatitud(float latitud) {
+		this.latitud = latitud;
+	}
+	public float getLongitud() {
+		return longitud;
+	}
+	public void setLongitud(float longitud) {
+		this.longitud = longitud;
+	}
+	public String getFoto() {
+		return foto;
+	}
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+		
+}
