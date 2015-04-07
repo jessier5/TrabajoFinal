497ff9dbe908878687a2d5db7133a254aba055a3
jessier5 <jessier1319@gmail.com>
2015-04-07 13:26:42 +0200

    cambios en el PrincipalActivity, manifest y layout

--- EjercicioFinal/src/es/exitae/ejerciciofinal/activity/PrincipalActivity.java
+++ EjercicioFinal/src/es/exitae/ejerciciofinal/activity/PrincipalActivity.java
@@ -1,7 +1,32 @@
 package es.exitae.ejerciciofinal.activity;
 
+import es.exitae.ejerciciofinal.R;
 import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class PrincipalActivity extends Activity {
+public class PrincipalActivity extends Activity implements OnClickListener {
+	
+	private Button btnLista, btnMapa;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main_activity);
+		
+		btnLista = (Button) findViewById(R.id.btnLista);
+		btnLista.setOnClickListener(this);
+		
+		
+		btnMapa = (Button) findViewById(R.id.btnMapa);
+		btnMapa.setOnClickListener(this);
+	}
 
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		
+	}
 }
